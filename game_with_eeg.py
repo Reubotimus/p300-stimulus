3c9b3c3f864e1b3cb1865c6319493f802ed53f36
NKLam <khoalamvn@gmail.com>
2022-08-28 10:03:46 +1000

    Capitalize constant

--- game_with_eeg.py
+++ game_with_eeg.py
@@ -5,20 +5,20 @@ import explorepy
 import argparse
 
 # adjustable variables
-stimulus_interval = 1.2  # time in s between each row / column
-intensification_duration = 0.1  # time that a row is intensified for
+STIMULUS_INTERVAL = 0.25  # time in s between each row / column
+INTENSIFICATION_DURATION = 0.1  # time that a row is intensified for
 # number of times each row and column will be cycled through before a break
-n_cycles_in_epoch = 1
+N_CYCLES_IN_EPOCH = 5
 # determines if the break is automatic or epoch is reinitiated by user pressing space
-auto_epoch = True
-break_time = 4  # break time between epochs
-displayed_chars = "123456789".upper()
-matrix_dimensions = (3, 3)
-font_size = 90
-grey = (50, 50, 50)
-intense = False
-# screen_size = (1280, 750)
-screen_size = (800, 600)
+AUTO_EPOCH = True
+BREAK_TIME = 4  # break time between epochs
+DISPLAYED_CHARS = "123456789".upper()
+MATRIX_DIMENSIONS = (3, 3)
+FONT_SIZE = 120
+GREY = (50, 50, 50)
+INTENSE = False
+# SCREEN_SIZE = (1280, 750)
+SCREEN_SIZE = (1600, 900)
 
 # We create the parser and explorer to record the data from the new EEG device
 
@@ -27,8 +27,10 @@ screen_size = (800, 600)
 # WARNING: We used the int() type caster in the explore.setMarker. This can Cause an issue in the future if you use letters. Letter -> Mapping can be used in the future
 # WARNING !!!!!!!!!
 
-parser = argparse.ArgumentParser(description="Example code for marker generation")
-parser.add_argument("-n", "--name", dest="name", type=str, help="Name of the device.")
+parser = argparse.ArgumentParser(
+    description="Example code for marker generation")
+parser.add_argument("-n", "--name", dest="name",
+                    type=str, help="Name of the device.")
 parser.add_argument(
     "-f",
     "--filename",
@@ -49,22 +51,24 @@ explore.record_data(
 
 # finds derived values
 char_surface_size = min(
-    (screen_size[0] / matrix_dimensions[0], screen_size[1] / matrix_dimensions[1])
+    (SCREEN_SIZE[0] / MATRIX_DIMENSIONS[0],
+     SCREEN_SIZE[1] / MATRIX_DIMENSIONS[1])
 )
-starting_x_pos = (screen_size[0] - char_surface_size * matrix_dimensions[0]) / 2
+starting_x_pos = (
+    SCREEN_SIZE[0] - char_surface_size * MATRIX_DIMENSIONS[0]) / 2
 
 # Initialises the pygame screen
 pygame.init()
-screen = pygame.display.set_mode(screen_size)
+screen = pygame.display.set_mode(SCREEN_SIZE)
 clock = pygame.time.Clock()
-font = pygame.font.Font("HelveticaBold.ttf", font_size)
+font = pygame.font.Font("HelveticaBold.ttf", FONT_SIZE)
 
 
 # creates class for characters being displayed on speller
 class Character:
     def __init__(self, character, position):
         self.character = character
-        self.character_image = font.render(character, True, grey)
+        self.character_image = font.render(character, True, GREY)
         self.image_location = (
             (char_surface_size - self.character_image.get_width()) / 2,
             (char_surface_size - self.character_image.get_height()) / 2,
@@ -74,7 +78,7 @@ class Character:
         self.surface.blit(self.character_image, self.image_location)
 
     def intensify(self):
-        if intense == True:
+        if INTENSE == True:
             self.surface.fill("white")
             self.character_image = font.render(self.character, True, "black")
             self.surface.blit(self.character_image, self.image_location)
@@ -87,24 +91,24 @@ class Character:
 
     def darken(self):
         self.surface.fill("black")
-        self.character_image = font.render(self.character, True, grey)
+        self.character_image = font.render(self.character, True, GREY)
         self.surface.blit(self.character_image, self.image_location)
 
 
 # Initialises all the characters to be shown on screen, saves each character in their group
 # each group is either a row or column
 chars = []
-# groups = [[] for i in range(matrix_dimensions[0] + matrix_dimensions[1])]
+# groups = [[] for i in range(MATRIX_DIMENSIONS[0] + MATRIX_DIMENSIONS[1])]
 i = 1
 row = 0
 col = 0
 pos = [starting_x_pos, 0]
 # pos2 = (starting_x_pos, 0)
-for char in displayed_chars:
+for char in DISPLAYED_CHARS:
     chars.append(Character(char, tuple(pos)))
     # groups[row].append(i - 1)
-    # groups[col + matrix_dimensions[0]].append(i - 1)
-    if i % matrix_dimensions[0] == 0 and i != 0:
+    # groups[col + MATRIX_DIMENSIONS[0]].append(i - 1)
+    if i % MATRIX_DIMENSIONS[0] == 0 and i != 0:
         pos[0] = starting_x_pos
         pos[1] += char_surface_size
         col = 0
@@ -135,7 +139,7 @@ while True:
             exit()
         # restarts epoch if user presses space
         if (
-            not auto_epoch
+            not AUTO_EPOCH
             and not epoch_on
             and event.type == pygame.KEYDOWN
             and event.key == pygame.K_SPACE
@@ -144,15 +148,15 @@ while True:
             n_cycles = 0
 
     # restarts epoch if on automatic and enough time has passed
-    if auto_epoch and not epoch_on and time_of_frame - time_end_epoch > break_time:
+    if AUTO_EPOCH and not epoch_on and time_of_frame - time_end_epoch > BREAK_TIME:
         epoch_on = True
         n_cycles = 0
 
     if epoch_on:
         # intensifies new group of chars and puts them on the screen
-        if time_of_frame - time_since_intensification > stimulus_interval:
+        if time_of_frame - time_since_intensification > STIMULUS_INTERVAL:
             # shuffles groups and starts again if all groups have been intensified
-            if group_num == matrix_dimensions[0] * matrix_dimensions[1]:
+            if group_num == MATRIX_DIMENSIONS[0] * MATRIX_DIMENSIONS[1]:
                 # ensures that the intensified row / col is not flashed consecutively
                 shuffled = False
                 last_char = chars[group_num - 1]
@@ -174,14 +178,14 @@ while True:
             row_intensified = True
             time_since_intensification = time.time()
             # ends epoch if completed n_cycles in epoch
-            if n_cycles >= n_cycles_in_epoch:
+            if n_cycles >= N_CYCLES_IN_EPOCH:
                 epoch_on = False
                 time_end_epoch = time.time()
 
     # darkens rows / cols after they have been on longer then intensification durantion
     if (
         row_intensified
-        and time_of_frame - time_since_intensification > intensification_duration
+        and time_of_frame - time_since_intensification > INTENSIFICATION_DURATION
     ):
         # darkens char
         chars[group_num - 1].darken()
